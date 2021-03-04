42957d07f921489c72690ccf73ad3ff91904ba25
Ryan M <RyanAMacLeod@hotmail.com>
2021-03-03 22:40:42 -0800

    revision one
    Added the Bandwidth Class (empty)

---
+++ src/Model/Bandwidth.java
@@ -0,0 +1,13 @@
+package Model;
+
+
+/**
+ * Bandwidth is an abstract data type designed to represent different bandwidths in bit/s
+ * */
+public class Bandwidth {
+    private double value;
+
+    public Bandwidth(double value, String unit){
+        this.value = value;
+    }
+}
