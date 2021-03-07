fe783843367dc9b8100313a94fe4841077e433d7
Ryan M <RyanAMacLeod@hotmail.com>
2021-03-07 11:01:04 -0800

    Revert "test revision two"
    This reverts commit 5a73d316b48c9188abe26130d040e1b30f08898b.

--- src/Controller/Validator.java
+++
@@ -1,8 +0,0 @@
-package Controller;
-
-public class Validator {
-    public boolean isDecimal(String str){
-        try { Integer.parseInt(str); return true; }
-        catch (NumberFormatException nfe){ return false; }
-    }
-}
